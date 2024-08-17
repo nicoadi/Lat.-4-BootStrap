d67cfd9a0601229e7d442010e08d381fe835066e
Nicodemus <nicodemusadiw@gmail.com>
2024-08-17 16:38:39 +0700

    FINAL

---
+++ MyJavaScript.js
@@ -0,0 +1,7 @@
+function testFunction() {
+    window.alert("Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, aperiam? ");
+}
+
+function submitFunction() {
+    window.alert("Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, aperiam? ");
+}
